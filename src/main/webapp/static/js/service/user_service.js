c331c42f68564b1d252b156162dd7bd4dc6424d6
skonduru3 <aws.srinivas@gmail.com>
2016-10-18 12:11:22 +0530

    relative paths

--- src/main/webapp/static/js/service/user_service.js
+++ src/main/webapp/static/js/service/user_service.js
@@ -2,7 +2,8 @@
 
 angular.module('myApp').factory('UserService', ['$http', '$q', function($http, $q){
 
-    var REST_SERVICE_URI = 'http://localhost:8080/demoweb/user/';
+    //var REST_SERVICE_URI = 'http://localhost:8080/demoweb/user/';
+	var REST_SERVICE_URI = 'user/';
 
     var factory = {
         fetchAllUsers: fetchAllUsers,
