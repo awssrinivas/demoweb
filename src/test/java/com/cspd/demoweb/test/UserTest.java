68584eaa2f7b165b6d06902761129396116709cf
skonduru3 <aws.srinivas@gmail.com>
2016-10-19 12:14:10 +0530

    JUnit test cases added

---
+++ src/test/java/com/cspd/demoweb/test/UserTest.java
@@ -0,0 +1,42 @@
+package com.cspd.demoweb.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.websystique.springmvc.model.User;
+import com.websystique.springmvc.service.UserService;
+import com.websystique.springmvc.service.UserServiceImpl;
+
+
+public class UserTest {
+	
+	private static UserService userService;
+	
+	@BeforeClass
+	public static void init() {
+		userService = new UserServiceImpl();
+	}	
+	
+	@Test
+	public void testUsers() {
+		List<User> result = userService.findAllUsers();
+		assertEquals(7, result.size());
+	}	
+		
+	@Before
+	public void beforeEachTest() {
+		System.out.println("This is executed before each Test");
+	}
+
+	@After
+	public void afterEachTest() {
+		System.out.println("This is exceuted after each Test");
+	}	
+	
+}
